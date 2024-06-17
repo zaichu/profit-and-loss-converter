82b3cedb7d47fb1b4225f055dc41baa4622841b0
Tomoya Zaitsu <zaitomo41@gmail.com>
2024-06-18 00:19:06 +0900

    ProfitAndLoss構造体に値をそれぞれ入れてみる

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
-use std::env;
-use std::path::Path;
-
+use clap::Parser;
+use std::error::Error;
+use std::path::PathBuf;
 pub mod modules;
 use modules::profit_and_loss_csv;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+#[derive(Parser)]
+struct Args {
+    #[clap(name = "FILE")]
+    csv_file: Option<PathBuf>,
+}
 
-    for (index, arg) in args.iter().enumerate() {
-        println!("Argument {index}: {arg}");
-        if index == 1 {
-            let path = Path::new(arg);
-            let result = profit_and_loss_csv::read_csv(path);
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
 
-            println!("{result:?}");
-        }
+    if let Some(csv_file) = args.csv_file {
+        profit_and_loss_csv::execute(&csv_file);
+    } else {
+        return Err(
+            "引数が不足しています。使用例: ./profit-and-loss-converter hogehoge.csv".into(),
+        );
     }
+
+    Ok(())
 }
--- src/modules/profit_and_loss_csv.rs
+++ src/modules/profit_and_loss_csv.rs
@@ -1,4 +1,6 @@
 use chrono::NaiveDate;
+use csv::StringRecord;
+use std::error::Error;
 use std::path::Path;
 
 #[derive(Debug, Clone)]
@@ -9,18 +11,76 @@ pub struct ProfitAndLoss {
     pub company_name: String,       // 銘柄名
     pub account: String,            // 口座
     pub shares: i32,                // 数量[株]
-    pub asked_price: i32,           // 売却/決済単価[円]
+    pub asked_price: f64,           // 売却/決済単価[円]
     pub settlement_amount: i32,     // 売却/決済額[円]
-    pub purchase_price: i32,        // 平均取得価額[円]
+    pub purchase_price: f64,        // 平均取得価額[円]
     pub profit_and_loss: i32,       // 実現損益[円]
 }
 
-pub fn read_csv(path: &Path) -> Result<(), csv::Error> {
-    let mut reader = csv::Reader::from_path(path)?;
-    for result in reader.records() {
-        let data = result?;
-        println!("{data:?}");
+impl ProfitAndLoss {
+    pub fn new(record: StringRecord) -> Result<Self, Box<dyn Error>> {
+        Ok(ProfitAndLoss {
+            trade_date: Self::parse_date("trade_date", record.get(0))?,
+            settlement_date: Self::parse_date("settlement_date", record.get(1))?,
+            securities_code: Self::parse_string("securities_code", record.get(2))?,
+            company_name: Self::parse_string("company_name", record.get(3))?,
+            account: Self::parse_string("account", record.get(4))?,
+            shares: Self::parse_int("shares", record.get(7))?,
+            asked_price: Self::parse_float("asked_price", record.get(8))?,
+            settlement_amount: Self::parse_int("settlement_amount", record.get(9))?,
+            purchase_price: Self::parse_float("purchase_price", record.get(10))?,
+            profit_and_loss: Self::parse_int("profit_and_loss", record.get(11))?,
+        })
     }
 
-    Ok(())
+    fn parse_date(field_name: &str, date_str: Option<&str>) -> Result<NaiveDate, Box<dyn Error>> {
+        let date_str = date_str.ok_or_else(|| format!("Missing {field_name} field"))?;
+        NaiveDate::parse_from_str(date_str.replace("/", "-").as_str(), "%Y-%m-%d")
+            .map_err(|e| format!("Failed to parse date '{date_str}': {e}").into())
+    }
+
+    fn parse_int(field_name: &str, num_str: Option<&str>) -> Result<i32, Box<dyn Error>> {
+        let num_str = num_str.ok_or_else(|| format!("Missing {field_name} field"))?;
+        num_str
+            .replace(",", "")
+            .parse::<i32>()
+            .map_err(|e| format!("Failed to parse integer '{num_str}': {e}").into())
+    }
+
+    fn parse_float(field_name: &str, num_str: Option<&str>) -> Result<f64, Box<dyn Error>> {
+        let num_str = num_str.ok_or_else(|| format!("Missing {field_name} field"))?;
+        num_str
+            .replace(",", "")
+            .parse::<f64>()
+            .map_err(|e| format!("Failed to parse float '{num_str}': {e}").into())
+    }
+
+    fn parse_string(field_name: &str, value: Option<&str>) -> Result<String, Box<dyn Error>> {
+        value
+            .ok_or_else(|| format!("Missing {field_name} field"))?
+            .to_string();
+        Ok(value.unwrap().to_string())
+    }
+}
+
+pub fn execute(path: &Path) {
+    match read_profit_and_loss(path) {
+        Ok(result) => {
+            for profit_and_loss in result {
+                println!("{profit_and_loss:?}");
+            }
+        }
+        Err(err) => println!("エラー: {err}"),
+    }
+}
+
+fn read_profit_and_loss(path: &Path) -> Result<Vec<ProfitAndLoss>, Box<dyn Error>> {
+    let mut result = Vec::new();
+    let mut reader = csv::Reader::from_path(path)?;
+
+    for record in reader.records() {
+        result.push(ProfitAndLoss::new(record?)?);
+    }
+
+    Ok(result)
 }
